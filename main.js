f48758f9cf8dd95c2cac6cf9df4a4f38e691d35b
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-01-29 09:25:54 +0300

    feat(main.js): assigned values

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ deposit,
 mission,
 period;
 
+money = 1200;
+
+income = 'фриланс';
+
+addExpenses = 'интернет, такси, коммуналка';
+
+deposit = false;
+
+mission = 59915279351;
+
+period = 4;
+
 alert('JavaScript');
 
 console.log('Привет, мир!');
