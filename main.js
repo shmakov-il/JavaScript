acd0fbe24a98e611b9b3b8416cbd0977481100af
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-01-30 01:16:37 +0300

    fix(main.js): fixed bags

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // Объявление переменных и присваивание им значений
 const money = 1200,
-income = 'фриланс',
-addExpenses = 'Интернет, Такси, Коммуналка',
-deposit = false,
-mission = 59915279351,
-period = 4;
+  income = 'фриланс',
+  addExpenses = 'Интернет, Такси, Коммуналка',
+  deposit = false,
+  mission = 59915279351,
+  period = 4,
+  budgetDay = money / 30;
 
 // Вывод в консоль тип данных money, income, deposit
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 
 // Вывод в консоль длины строки addExpenses
 console.log(addExpenses.length);
@@ -21,12 +20,5 @@ console.log('Цель заработать ' + mission + ' евро');
 // Вывод в консоль строки addExpenses в нижнем регистре и ее разбиение на массив
 console.log(addExpenses.toLowerCase().split(', '));
 
-// Объявление новой переменной и присваивание ей значения
-const budgetDay = money / 30;
+// Вывод в консоль budgetDay
 console.log('budgetDay: ', budgetDay);
-
-// Вывод сообщения в модальном окне
-alert('JavaScript');
-
-// Вывод сообщения в консоль
-console.log('Привет, мир!');
