0f673706298cfb098436776aecd595f81438ed8c
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-03 01:59:15 +0300

    fix(main.js): correct mistakes

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 'use strict';
 
-// Объявление переменной mission 
-const mission = 150000;
+// Объявление переменных
+const mission = 150000,
+  money = prompt('Ваш месячный доход?'),
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  deposit = confirm('Есть ли у вас депозит в банке?'),
+  expenses1 = prompt('Введите обязательную статью расходов?'),
+  amount1 = prompt('Во сколько это обойдется?'),
+  expenses2 = prompt('Введите обязательную статью расходов?'),
+  amount2 = prompt('Во сколько это обойдется?'),
+  budgetMonth = Number(money) - Number(amount1) - Number(amount2),
+  compliteTarget = mission / budgetMonth,
+  budgetDay = budgetMonth / 30;
 
-// Спрашиваем у пользователя месячный доход
-const money = prompt('Ваш месячный доход?');
+
+// Выводим месячный доход
 console.log('Ваш месячный доход ', money);
 
-// Спрашиваем у пользователя возможные расходы
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+// Выводим возможные расходы
 console.log('Расходы: ', addExpenses);
 
-// Спрашиваем у пользователя про депозит
-const deposit = confirm('Есть ли у вас депозит в банке?');
+// Выводим информацию про депозит
 console.log('Наличие депозита: ', deposit);
 
-// Спрашиваем у пользователя два раза про обязательную статью расходов
-const expenses1 = prompt('Введите обязательную статью расходов?');
+// Выводим информацию про обязательные расходы
 console.log('Расход 1: ', expenses1);
-const expenses2 = prompt('Введите обязательную статью расходов?');
-console.log('Расход 2: ', expenses2);
-
-// Спрашиваем у пользователя про величину этих двух расходов
-const amount1 = prompt('Во сколько это обойдется?');
 console.log('Величина 1: ', amount1);
-const amount2 = prompt('Во сколько это обойдется?');
+console.log('Расход 2: ', expenses2);
 console.log('Величина 2: ', amount2);
 
-// Вычисляем бюджет на месяц
-const budgetMonth = Number(money) - Number(amount1) - Number(amount2);
+// Выводим бюджет на месяц
 console.log('Месячный бюджет: ', budgetMonth);
 
-// Расчет compliteTarget, за сколько будет выполнена цель mission
-const compliteTarget = mission / budgetMonth;
+// Выводим compliteTarget, за сколько будет выполнена цель mission
 console.log('Цель будет достигнута за ' + Math.ceil(compliteTarget) + ' месяцев (-а)');
 
-// Вычисляем дневной бюджет - budgetDay
-const budgetDay = budgetMonth / 30;
+// Выводим дневной бюджет - budgetDay
 console.log('Дневной бюджет: ', Math.floor(budgetDay));
 
 // Пишем конструкцию условий
@@ -44,7 +43,7 @@ switch (true) {
   case (budgetDay >= 1200):
     console.log('У вас высокий уровень дохода');
     break;
-  case (budgetDay <= 600 && budgetDay > 0):
+  case (budgetDay <= 600 && budgetDay >= 0):
     console.log('К сожалению, у вас уровень дохода ниже среднего');
     break;
   case (budgetDay < 1200 && budgetDay > 600):
